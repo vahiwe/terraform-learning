88c701caac39f6f7f48a6af9a058653e5cedd8e9
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2022-06-27 10:30:38 +0100

    Deploying a Load Balancer

--- main.tf
+++ main.tf
@@ -41,6 +41,9 @@ resource "aws_autoscaling_group" "web_asg" {
     launch_configuration = aws_launch_configuration.web_launch_configuration.name
     vpc_zone_identifier = data.aws_subnets.default.ids
 
+    target_group_arns = [aws_lb_target_group.asg.arn]
+    health_check_type = "ELB"
+
     min_size = 2
     max_size = 10
     
@@ -50,8 +53,6 @@ resource "aws_autoscaling_group" "web_asg" {
         propagate_at_launch = true
         }
 }
-
-
 
 resource "aws_security_group" "web_sg" {
     name = "terraform-example-web-sg"
@@ -63,4 +64,86 @@ resource "aws_security_group" "web_sg" {
       protocol = "tcp"
       to_port = var.server_port
     } 
+}
+
+resource "aws_lb" "web_alb" {
+    name = "terraform-example-web-alb"
+    subnets = data.aws_subnets.default.ids
+    load_balancer_type = "application"
+    security_groups = [ aws_security_group.web_alb.id ]
+}
+
+resource "aws_lb_listener" "web_alb_listener" {
+    load_balancer_arn = aws_lb.web_alb.arn
+    protocol = "HTTP"
+    port = "80"
+
+    # By default, return a simple 404 page
+    default_action {
+      type = "fixed-response"
+
+      fixed_response {
+        content_type = "text/plain"
+        message_body = "404: page not found"
+        status_code  = 404
+      }
+    }
+}
+
+resource "aws_security_group" "web_alb" {
+  name = "terraform-example-alb"
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.web_alb_listener.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+output "alb_dns_name" {
+  value       = aws_lb.web_alb.dns_name
+  description = "The domain name of the load balancer"
 }
