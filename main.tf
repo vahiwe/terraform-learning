c94974672b49347fbc9f7de208041fad1171d0e5
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2022-06-27 08:07:47 +0100

    Deploy a Configurable Web Server

--- main.tf
+++ main.tf
@@ -2,6 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "server_port" {
+    default = 8080
+    type = number
+    description = "The port the server will use for HTTP requests"
+}
+
 resource "aws_instance" "web" {
   instance_type = "t2.micro"
   ami = "ami-08d4ac5b634553e16"
@@ -11,7 +17,7 @@ resource "aws_instance" "web" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   tags = {
@@ -25,8 +31,13 @@ resource "aws_security_group" "web_sg" {
     ingress {
       cidr_blocks = [ "0.0.0.0/0" ]
       description = "Allow all from anywhere to port 8080"
-      from_port = 8080
+      from_port = var.server_port
       protocol = "tcp"
-      to_port = 8080
+      to_port = var.server_port
     } 
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+  description = "The public IP address of the web server"
 }
