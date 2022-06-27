032139880a87e0ab8aa2f4e68d12f519fa5d87e7
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2022-06-27 11:18:12 +0100

    Limitations with Terraform’s Backends

--- chapterThree/main.tf
+++ chapterThree/main.tf
@@ -1,13 +1,9 @@
+# Partial configuration. The other settings (e.g., bucket, region) will be
+# passed in from a file via -backend-config arguments to 'terraform init'
+# terraform init -backend-config=backend.hcl
 terraform {
   backend "s3" {
-    # Replace this with your bucket name!
-    bucket         = "terraform-up-and-running-state-593493008121"
     key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
-
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
   }
 }
 
